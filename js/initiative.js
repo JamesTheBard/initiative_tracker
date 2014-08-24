fe32db7bf2cebfe32f82609e685facb8fc226a7d
Jason Weatherly <jamesthebard@gmail.com>
2014-08-24 02:56:28 -0400

    Version number bump.

--- js/initiative.js
+++ js/initiative.js
@@ -1,4 +1,4 @@
-var version = "Version 1.2.2 by Jason Weatherly";
+var version = "Version 1.2.3 by Jason Weatherly";
 var title = "Initiative Tracker";
 var db = window.openDatabase("UsersDB", "", "UserTable", 1024*1000);
 var round = 0;
