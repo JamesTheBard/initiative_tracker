812fe715a1839663c97685eb55a34c5d67c85882
Jason Weatherly <jamesthebard@gmail.com>
2014-09-07 01:20:00 +0000

    Implemented "Effects" system, some bugfixes.

--- js/initiative.js
+++ js/initiative.js
@@ -1,4 +1,4 @@
-var version = "Version 1.2.3 by Jason Weatherly";
+var version = "Version 1.2.999 by Jason Weatherly";
 var title = "Initiative Tracker";
 var db = window.openDatabase("UsersDB", "", "UserTable", 1024*1000);
 var round = 0;
@@ -12,19 +12,27 @@ var insertStatement = "INSERT INTO Characters (player_name, initiative, bonus, d
 var deleteStatement = "DELETE FROM Characters WHERE player_name=?";
 
 /* For the effect tracker */
-var createEffectTable = "CREATE TABLE IF NOT EXISTS Effects (id INTEGER PRIMARY KEY AUTOINCREMENT, player_name TEXT, round INT, short TEXT, description TEXT)";
-var selectAllEffectTable = "SELECT * FROM Effects ORDER BY round ASC, short ASC";
-var insertEffectTable = "INSERT INTO Effects (player_name, round, short, description) VALUES (?, ?, ?, ?)";
+var createEffectTable = "CREATE TABLE IF NOT EXISTS Effects (id INTEGER PRIMARY KEY AUTOINCREMENT, player_name TEXT, round INT, short TEXT, description TEXT, type TEXT)";
+var insertEffect = "INSERT INTO Effects (player_name, round, short, description, type) VALUES (?, ?, ?, ?, ?)"; 
+var selectAllEffects = "SELECT * FROM Effects ORDER BY round ASC, short ASC";
+var deleteEffect = "DELETE FROM Effects WHERE id=?";
 
 createTable();
 refreshView();
 
 $('div.round').hide();
+$('div.triggers').hide();
 
 $('#submit_new_player').click( function() {
     console.log("Adding player to local DB.");
     addCharacter();
     $('form[name="addCharacter"]').find("input").val("");
+    });
+
+$('#submit_new_effect').click( function() {
+    console.log("Adding effect to local DB.");
+    addEffect();
+    $('form[name="addEffect"]').find("input").val("");
     });
 
 function nullDataHandler(transaction, results) { }
@@ -38,16 +46,17 @@ function createTable() {
 	});
 }
 
-/* var insertEffectTable = "INSERT INTO Effects (player_name, round, short, description) VALUES (?, ?, ?, ?)"; */
 function addEffect() {
-	var player_name, round, short, description;
+	var player_name, round, short, description, type;
 	var fields = $('form[name="addEffect"]').serializeArray();
 	$.each(fields, function(i, fd){
 		if(fd.name === "player_name") player_name = fd.value;
 		if(fd.name === "effect_name") short = fd.value;
 		if(fd.name === "round") round = fd.value;
 		if(fd.name === "description") description = fd.value;
-	})
+        if(fd.name === "whose_character") type = fd.value;
+        console.log("fd.name: " + fd.name + " [" + fd.value + "]");
+	});
 	if (!player_name || !round || !short || !description) {
 		console.log("Missing a field, try again.");
 		alert("You're missing a field, try again.");
@@ -55,7 +64,7 @@ function addEffect() {
 	}
 	db.transaction(function (tx) {
 		tx.executeSql(insertEffect,
-			[player_name, round, short, description]);
+			[player_name, round, short, description, type]);
 	});
 	refreshView();
 }
@@ -123,17 +132,33 @@ function dataHandler(transaction, results) {
     addDeleteClickEvent();
 }
 
-function dataHandlerEffect(transaction, results) {
-	var final_string = "";
+/* var insertEffectTable = "INSERT INTO Effects (player_name, round, short, description, type) VALUES (?, ?, ?, ?, ?)"; */
+function dataHandlerEffects(transaction, results) {
+	var final_content = "";
 	var source = $("#effect-template").html();
 	var template = Handlebars.compile(source);
 	
-	if (results.rows.length > 0) {
+	if (results.rows.length >= 0) {
 		for (var i=0; i<results.rows.length; i++) {
 			var row = results.rows.item(i);
-			typelabel = (())
+            delay_class = ((row['round'] >= round ) ? 'delay' : 'delay inactive');
+            console.log(delay_class + "|" + round);
+			typelabel = ((row['type'] === 'pc') ? 'pc' : 'npc');
+            var context = {
+                round: row["round"],
+                source: row["player_name"],
+                description: row["description"],
+                effect: row["short"],
+                type: typelabel,
+                delay: delay_class,
+                id: row['id']
+            };
+            final_content = final_content + template(context);                       
 		}
+        $('div.triggers').html(final_content);
+        updatePlayer();
 	}
+    addDeleteEffectEvent();
 }
 
 function addOnClickEvent() {
@@ -159,6 +184,22 @@ function addDeleteClickEvent() {
     });
 }
 
+function addDeleteEffectEvent() {
+    $('div').on('mouseenter', 'div.is_icon', function(){
+        $(this).addClass("highlight_icon");
+    });
+    $('div').on('mouseleave', 'div.is_icon', function() {
+        $(this).removeClass("highlight_icon");
+    });
+    $('div').on('click', 'div.remove_effect', function() {
+        id = $(this).attr('effect');
+        db.transaction(function(tx) {
+            tx.executeSql(deleteEffect, [id]);
+        });
+        refreshView();
+    });
+}
+
 $('#start').click(function(){
     startRounds();
 });
@@ -170,6 +211,7 @@ $('#next_button').click(function(){
 // The Hotkey part
 $(document).keypress(function(e){
     if ($('form[name="addCharacter"]').is(':visible')) return;
+    if ($('form[name="addEffect"]').is(':visible')) return;
     switch(e.which) {
         case 110: // Letter 'n'
         case 32: // Spacebar
@@ -208,11 +250,13 @@ $('button[name="about"]').popover({
 function startRounds() {
     if (no_players) return; 
     is_started = true;
+    $('button.add-effect-btn').prop('disabled', false);
     current_player = 1;
     $('.player').first().addClass('selected');
     round = 1;
     $('#round_text').text(round);
     $('div.round').show();
+    $('div.triggers').show();
 }
 
 function nextPlayer() {
@@ -224,6 +268,7 @@ function nextPlayer() {
             current_player++;
         }
         updatePlayer();
+        updateEffects();
     }
 }
 
@@ -236,7 +281,19 @@ function prevPlayer() {
             current_player--;
         }
         updatePlayer();
+        updateEffects();
     }
+}
+
+function updateEffects() {
+    $(".triggers .delay").each(function(i) {
+        if ($( this ).find(".round").text() < round) {
+            $( this ).addClass( "inactive" );
+        }
+        else {
+            $( this ).removeClass( "inactive" );
+        }
+    });
 }
 
 function resetPlayer(){
@@ -246,6 +303,8 @@ function resetPlayer(){
     is_started = false;
     $('#round_text').text(round);
     $('div.round').hide();
+    $('div.triggers').hide();
+    $('button.add-effect-btn').prop('disabled', true);
 }
 
 function updatePlayer(){
